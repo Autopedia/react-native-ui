843c9376a01a493410c6bf6e1175e488e15d9847
seungsu-autopedia <seungsu.kim@autopediacar.com>
2021-05-10 20:12:36 +0900

    :pencil: Add TODOs to README

---
+++ README.md
@@ -0,0 +1,7 @@
+A React Native UI components for React Native app services of doctor-cha.
+
+# TODOs
+
+- [ ] `styled-component` theme configuration
+- [ ] `@storybook/addon-storyshots` snapshot testing configuration
+- [ ] Unit testing with `Jest`
--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { TouchableHighlight } from 'react-native';
-
-// export default function Button({ onPress, children }) {
-//   return <TouchableHighlight onPress={onPress}>{children}</TouchableHighlight>;
-// }
-
-// Button.defaultProps = {
-//   children: null,
-//   onPress: () => {},
-// };
-
-// Button.propTypes = {
-//   children: PropTypes.node,
-//   onPress: PropTypes.func,
-// };
-
 import React from 'react';
 import { GestureResponderEvent } from 'react-native';
 import styled from 'styled-components/native';
