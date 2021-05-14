a28b3979b6c2f60051b5dc7c62385a4445fdcc05
Gwangjo Gong <dwc05101@kaist.ac.kr>
2021-05-14 16:46:30 +0900

    :pencil: Wrote comments on button props: layout, absoluteIcon

--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -14,14 +14,28 @@ import Icon from '@atoms/Icon';
 import lodash from 'lodash';
 
 interface ButtonProps {
+  /**
+   * layout (default : inline)
+   * @inline    button width determined by the inner text
+   * @block     button expands to maximum length possible
+   * @sticky    button expands to maximum length and fixed at the bottom
+   */
   layout?: ButtonLayout;
+
   type?: ButtonType;
   size?: ButtonSize;
   color?: ButtonColor;
   loading?: boolean;
   disabled?: boolean;
+
   icon?: any;
   iconPosition?: ButtonIconPosition;
+
+  /**
+   * absoluteIcon (default: false)
+   * !!NOT compatible with DEFAULT layout button!!
+   * @true    position of the icon fixed to one side of the button (corresponding to iconPosition)
+   */
   absoluteIcon?: boolean;
   style?: ViewStyle;
   onPress: (event: GestureResponderEvent) => void;
