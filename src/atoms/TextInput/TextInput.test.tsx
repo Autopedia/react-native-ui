e5963b927831292dc31ca0d491285b63763ae8d6
Gwangjo Gong <dwc05101@kaist.ac.kr>
2021-05-17 17:46:36 +0900

    :white_check_mark: Updated unit test

--- src/atoms/TextInput/TextInput.test.tsx
+++ src/atoms/TextInput/TextInput.test.tsx
@@ -10,29 +10,53 @@ import { shallow } from 'enzyme';
 
 describe('[TextInput] Unit Test', () => {
   const TEST_TEXT = 'test';
+  const TEST_TEXT_NUMERIC = '1111';
+  const TEST_TEXT_MIXED = '1a1b1c1d';
 
   it('should fire onChangeText event', () => {
     const onChangeTextMock = jest.fn();
+
     const wrapper = shallow(<TextInput onChangeText={onChangeTextMock} />);
     const textInput = wrapper.find('Styled(TextInput)');
-
     textInput.simulate('changeText', TEST_TEXT);
+    expect(onChangeTextMock).toHaveBeenCalledTimes(1);
     expect(onChangeTextMock).toHaveBeenCalledWith(TEST_TEXT);
   });
 
-  it('should change binded value on input', () => {
-    let textInputValue = 'oldValue';
-    const onChangeText = (newValue: string) => {
-      textInputValue = newValue;
-    };
+  it('should not change binded value when disabled', () => {
+    let value = 'original';
+    const onChangeText = (newValue: string) => (value = newValue);
 
     const wrapper = shallow(
-      <TextInput value={textInputValue} onChangeText={onChangeText} />,
+      <TextInput disabled value={value} onChangeText={onChangeText} />,
     );
     const textInput = wrapper.find('Styled(TextInput)');
+    textInput.simulate('changeText', TEST_TEXT);
 
-    expect(textInput).toHaveLength(1);
+    expect(value).toEqual('original');
+  });
+
+  it('should reject non-numeric input when keyboard type is numeric', () => {
+    let value = '1234';
+    const onChangeText = (newValue: string) => (value = newValue);
+
+    const wrapper = shallow(
+      <TextInput
+        value={value}
+        keyboardType="numeric"
+        onChangeText={onChangeText}
+      />,
+    );
+
+    const textInput = wrapper.find('Styled(TextInput)');
 
     textInput.simulate('changeText', TEST_TEXT);
+    expect(value).toEqual('1234');
+
+    textInput.simulate('changeText', TEST_TEXT_NUMERIC);
+    expect(value).toEqual(TEST_TEXT_NUMERIC);
+
+    textInput.simulate('changeText', TEST_TEXT_MIXED);
+    expect(value).toEqual(TEST_TEXT_NUMERIC);
   });
 });
