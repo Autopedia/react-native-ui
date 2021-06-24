62a39856a06dee50badd683dc167a6025fb1a358
Gwangjo Gong <dwc05101@kaist.ac.kr>
2021-06-24 16:18:07 +0900

    :sparkles: 시스템컬러 추가

--- src/styles/sytem-colors.ts
+++ src/styles/sytem-colors.ts
@@ -5,8 +5,12 @@ export const systemColors = {
   PRIMARY_EXTRALIGHT: '#E0ECFA',
   SUCCESS: '#48CF7C',
   SUCCESS_DARK: '#0FA364',
+  SUCCESS_LIGHT: '#B4F4C6',
+  SUCCESS_EXTRALIGHT: '#D8F4E1',
   ERROR: '#FA4339',
   ERROR_DARK: '#C5372F',
+  ERROR_LIGHT: '#FFB7B2',
+  ERROR_EXTRALIGHT: '#FFEAE9',
   BLACK: '#000000',
   WHITE: '#FFFFFF',
 };
@@ -18,11 +22,21 @@ export const systemColorMap = {
   primary_extralight: 'PRIMARY_EXTRALIGHT',
   success: 'SUCCESS',
   success_dark: 'SUCCESS_DARK',
+  success_light: 'SUCCESS_LIGHT',
+  success_extralight: 'SUCCESS_EXTRALIGHT',
   error: 'ERROR',
   error_dark: 'ERROR_DARK',
+  error_light: 'ERROR_LIGHT',
+  error_extralight: 'ERROR_EXTRALIGHT',
   black: 'BLACK',
   white: 'WHITE',
 };
 
 export type SystemColorKey = keyof typeof systemColors;
 export type SystemColor = keyof typeof systemColorMap;
+export type SystemColorMain =
+  | 'primary'
+  | 'success'
+  | 'error'
+  | 'black'
+  | 'white';
