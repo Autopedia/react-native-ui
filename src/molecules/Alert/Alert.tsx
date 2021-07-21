04930bffb208f70cbea2f48274c69266dc73c265
SungHoon-autopedia <87290584+SungHoon-autopedia@users.noreply.github.com>
2021-07-21 17:49:22 +0900

    molecules/Alert 개발 (#48)
    * molecules/Alert 개발
    
    * molecules/Alert 컴포넌트 개발
    
    * unit, snapshot 테스트 코드 작성
    
    * props contents -> content로 변경, 테스트를 위한 버튼 스타일 컴포넌트 display 네임 지정
    
    * 버튼 위치가 아닌 onPress Function name으로 구분, spyOn활용 코드 수정
    
    * prop contents -> content로 변경, 이벤트기능 storybook action으로 피드백 받도록 수정
    
    * 컴포넌트 title, content 테스트용 displayName추가
    
    * 버튼 텍스트, Title, Content 렌더 테스트 추가
    
    * 테스트 내부 mock 변수 추가 및 불필요 유닛테스트 제거

---
+++ src/molecules/Alert/Alert.tsx
@@ -0,0 +1,168 @@
+import React, { useImperativeHandle } from 'react';
+import { Dimensions, Modal as RNModal } from 'react-native';
+import Divider from '../../atoms/Divider';
+import styled, { css } from 'styled-components/native';
+import Typography from '../../atoms/Typography';
+import colors from '../../styles/colors';
+import { AlertHandle } from './Alert.types';
+
+export interface AlertProps {
+  content: string | React.ReactNode;
+  title?: string | React.ReactNode;
+  okText?: string;
+  onPressOk?: () => void;
+  okTextStrong?: boolean;
+  okTextColor?: string;
+  cancelText?: string;
+  onPressCancel?: () => void;
+  cancelTextStrong?: boolean;
+  cancelTextColor?: string;
+}
+
+const Alert = React.forwardRef<AlertHandle, AlertProps>((props, ref) => {
+  const [visible, setVisible] = React.useState(false);
+  const vertical = props.okText?.length >= 5 || props.cancelText?.length >= 5;
+  useImperativeHandle(
+    ref,
+    () => ({
+      open() {
+        setVisible(true);
+      },
+    }),
+    [],
+  );
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  const handleOk = () => {
+    if (props.onPressOk) props.onPressOk();
+    handleClose();
+  };
+
+  const handleCancel = () => {
+    if (props.onPressCancel) props.onPressCancel();
+    handleClose();
+  };
+
+  return (
+    <RNModal transparent visible={visible} onRequestClose={handleClose}>
+      <SAlertBackground>
+        <SAlertContainer>
+          <SAlertContent>
+            {props.title && (
+              <SAlertTitleView>
+                {typeof props.title === 'string' ? (
+                  <SAlertTitleText size={5}>{props.title}</SAlertTitleText>
+                ) : (
+                  props.title
+                )}
+              </SAlertTitleView>
+            )}
+            {typeof props.content === 'string' ? (
+              <SAlertContentText size={1}>{props.content}</SAlertContentText>
+            ) : (
+              props.content
+            )}
+          </SAlertContent>
+          <Divider margin={0} />
+          <SAlertFooter vertical={vertical}>
+            {!!props.cancelText && (
+              <>
+                <SConfirmButton onPress={handleCancel} vertical={vertical}>
+                  <SConfirmButtonText
+                    size={1}
+                    color={props.cancelTextColor}
+                    strong={props.cancelTextStrong}
+                  >
+                    {props.cancelText}
+                  </SConfirmButtonText>
+                </SConfirmButton>
+                {vertical ? <Divider margin={0} /> : <VerticalDivider />}
+              </>
+            )}
+            <SConfirmButton onPress={handleOk} vertical={vertical}>
+              <SConfirmButtonText
+                size={1}
+                color={props.okTextColor}
+                strong={props.okTextStrong}
+              >
+                {props.okText || '확인'}
+              </SConfirmButtonText>
+            </SConfirmButton>
+          </SAlertFooter>
+        </SAlertContainer>
+      </SAlertBackground>
+    </RNModal>
+  );
+});
+
+interface VerticalProps {
+  vertical: boolean;
+}
+
+interface ConfirmButtonTextProps {
+  color?: string;
+  strong?: boolean;
+}
+
+const SAlertBackground = styled.View`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
+const SAlertContainer = styled.View`
+  width: ${Dimensions.get('window').width - 60}px;
+  align-items: center;
+  border-radius: 20px;
+  background-color: ${colors.CARD};
+`;
+
+const SAlertContent = styled.View`
+  padding: 32px 16px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SAlertContentText = styled(Typography.Paragraph)`
+  text-align: center;
+`;
+SAlertContentText.displayName = 'ContentText';
+
+const SAlertTitleView = styled.View`
+  align-items: center;
+`;
+
+const SAlertTitleText = styled(Typography.Heading)``;
+SAlertTitleText.displayName = 'TitleText';
+
+const SAlertFooter = styled.View<VerticalProps>`
+  width: 100%;
+  flex-direction: ${({ vertical }) => (vertical ? 'column-reverse' : 'row')};
+`;
+
+const VerticalDivider = styled.View`
+  border-right-width: 1px;
+  border-right-color: ${colors.EXTRALIGHT};
+`;
+
+const SConfirmButton = styled.TouchableOpacity<VerticalProps>`
+  padding: 15px 0px;
+  justify-content: center;
+  align-items: center;
+  ${({ vertical }) => !vertical && `flex: 1;`}
+`;
+SConfirmButton.displayName = 'AlertButton';
+
+const SConfirmButtonText = styled(Typography.Paragraph)<ConfirmButtonTextProps>`
+  color: ${({ color }) => (color ? color : '#000')};
+  font-weight: ${({ strong }) => (strong ? 'bold' : 'normal')};
+`;
+
+export default Alert;
