0f1e3a69654243740aa66dc30aa6ed1cbd6600f3
sunghoon.moon <sunghoon.moon@autopediacar.com>
2021-07-21 15:38:31 +0900

    버튼 위치가 아닌 onPress Function name으로 구분, spyOn활용 코드 수정

--- src/molecules/Alert/Alert.test.tsx
+++ src/molecules/Alert/Alert.test.tsx
@@ -6,7 +6,7 @@ import 'react-native';
 import 'jest-styled-components';
 import React, { useRef } from 'react';
 import Alert from '.';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import { AlertHandle } from './Alert.types';
 import { AlertProps } from './Alert';
 
@@ -16,43 +16,64 @@ const AlertWrapper: React.FC<AlertProps> = props => {
   return (
     <Alert
       ref={alertRef}
+      content={props.content}
       title={props.title}
-      contents={props.contents}
-      onPressOk={props.onPressOk}
       okText={props.okText}
+      onPressOk={props.onPressOk}
+      okTextStrong={props.okTextStrong}
+      okTextColor={props.okTextColor}
       cancelText={props.cancelText}
       onPressCancel={props.onPressCancel}
+      cancelTextStrong={props.cancelTextStrong}
+      cancelTextColor={props.cancelTextColor}
     />
   );
+};
+
+const findButton = (
+  w: ShallowWrapper<any, any, React.Component<{}, {}, any>>,
+  eventName: string,
+) => {
+  const pressEvent = w.prop('onPress');
+  if (pressEvent) return pressEvent.name === eventName;
+  return false;
 };
 
 describe('[Alert] Unit Test', () => {
   it('should action without onPressOK, onPressCancel props', () => {
     const wrapper = shallow(
-      <AlertWrapper contents="Test Alert" cancelText="" />,
+      <AlertWrapper title="title" content="Test Alert" cancelText="닫기" />,
     );
     const Alert = wrapper.find('ForwardRef').dive();
-    const setState = jest.fn();
     const useStateSpy = jest.spyOn(React, 'useState');
 
-    useStateSpy.mockImplementation(() => ['', setState]);
-    Alert.find('Styled(Component)').first().simulate('press');
-    Alert.find('Styled(Component)').at(1).simulate('press');
+    const cancelButton = Alert.find('AlertButton').findWhere(w =>
+      findButton(w, 'handleCancel'),
+    );
+    const okButton = Alert.find('AlertButton').findWhere(w =>
+      findButton(w, 'handleOk'),
+    );
+    cancelButton.simulate('press');
+    okButton.simulate('press');
 
-    expect(setState).toBeCalled();
+    expect(useStateSpy).toBeCalled();
+    expect(useStateSpy).toHaveBeenCalledTimes(2);
   });
 
   it('should action onPressOk', () => {
     const onPressOkMock = jest.fn();
     const wrapper = shallow(
       <AlertWrapper
-        contents="Test Alert"
+        title="title"
+        content="Test Alert"
         onPressOk={onPressOkMock}
         okText="OK"
       />,
     );
     const Alert = wrapper.find('ForwardRef').dive();
-    Alert.find('Styled(Component)').simulate('press');
+    Alert.find('AlertButton')
+      .findWhere(w => findButton(w, 'handleOk'))
+      .simulate('press');
 
     expect(onPressOkMock).toHaveBeenCalledTimes(1);
   });
@@ -61,13 +82,17 @@ describe('[Alert] Unit Test', () => {
     const onPressCancelMock = jest.fn();
     const wrapper = shallow(
       <AlertWrapper
-        contents="Test Alert"
+        title="title"
+        content="Test Alert"
         onPressCancel={onPressCancelMock}
+        okText="OK"
         cancelText="Cancel"
       />,
     );
     const Alert = wrapper.find('ForwardRef').dive();
-    Alert.find('Styled(Component)').first().simulate('press');
+    Alert.find('AlertButton')
+      .findWhere(w => findButton(w, 'handleCancel'))
+      .simulate('press');
 
     expect(onPressCancelMock).toHaveBeenCalledTimes(1);
   });
