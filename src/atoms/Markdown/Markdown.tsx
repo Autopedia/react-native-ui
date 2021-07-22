6a1cf04718db9e7344839e96bbe4187119af41ff
sunghoon.moon <sunghoon.moon@autopediacar.com>
2021-07-22 15:02:53 +0900

    Markdown event함수 로직 수정

--- src/atoms/Markdown/Markdown.tsx
+++ src/atoms/Markdown/Markdown.tsx
@@ -16,32 +16,32 @@ interface MarkdownProps {
 
 export const Markdown: React.FC<MarkdownProps> = React.memo(
   ({ children, textColor = colors.ON_BACKGROUND, selectable }) => {
-  const mdTextStyle = markdownTextStyle(textColor);
-  return (
-    <ParsedText
-      selectable={selectable}
-      style={mdTextStyle.text}
-      parse={[
-        {
-          type: 'url',
-          style: mdTextStyle.url,
-          onPress: handleUrlPress,
-        },
-        {
-          pattern: BOLD_PATTERN,
-          style: mdTextStyle.bold,
-          renderText: renderBoldText,
-        },
-        {
-          pattern: PHONENUMBER_PATTERN,
-          style: mdTextStyle.phone,
-          onPress: handlePhonePress,
-        },
-      ]}
-    >
-      {children}
-    </ParsedText>
-  );
+    const mdTextStyle = markdownTextStyle(textColor);
+    return (
+      <ParsedText
+        selectable={selectable}
+        style={mdTextStyle.text}
+        parse={[
+          {
+            type: 'url',
+            style: mdTextStyle.url,
+            onPress: handleUrlPress,
+          },
+          {
+            pattern: BOLD_PATTERN,
+            style: mdTextStyle.bold,
+            renderText: renderBoldText,
+          },
+          {
+            pattern: PHONENUMBER_PATTERN,
+            style: mdTextStyle.phone,
+            onPress: handlePhonePress,
+          },
+        ]}
+      >
+        {children}
+      </ParsedText>
+    );
   },
 );
 
@@ -66,32 +66,18 @@ const markdownTextStyle = (textColor: string) => {
 
 const handleUrlPress = async (url: string) => {
   const valid = await Linking.canOpenURL(url);
-  if (valid) {
-    Linking.openURL(url);
-  } else {
+  if (valid) await Linking.openURL(url);
+  else
     Alert.alert(
       '링크 접속 불가',
       '연결중 문제가 발생했습니다. 직접 복사하여 브라우저에서 시도해주세요',
     );
-  }
 };
 
 const handlePhonePress = (phoneNumber: string) => {
-  if (phoneNumber) {
-    Linking.openURL(`tel:${phoneNumber}`);
-  } else {
-    Alert.alert(
-      '연락처 정보 없음',
-      '전화앱으로의 연결 중 문제가 발생했습니다. 직접 복사해서 사용해주세요.',
-    );
-  }
+  Linking.openURL(`tel:${phoneNumber}`);
 };
 
 const renderBoldText = (matchingString: string) => {
-  const match = matchingString.match(BOLD_PATTERN);
-  if (match) {
-    return `${match[0].replace(/\*\*(.*)\*\*/, '$1')}`;
-  } else {
-    return matchingString;
-  }
+  return `${matchingString.replace(/\*\*(.*)\*\*/, '$1')}`;
 };
