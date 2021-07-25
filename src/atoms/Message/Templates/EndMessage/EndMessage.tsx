ba5ca41df2df496fdbfbfc89522201ea98a1ee59
Hemistone <kei6040@gmail.com>
2021-07-25 17:43:31 +0900

    :bug: edit dependencies with Button

--- src/atoms/Message/Templates/EndMessage/EndMessage.tsx
+++ src/atoms/Message/Templates/EndMessage/EndMessage.tsx
@@ -3,6 +3,7 @@ import { Alert } from 'react-native';
 import { EndMessageProps } from '../../Message.types';
 import Button from '../../../../atoms/Button';
 import CardMessage from '../CardMessage';
+import { systemColors } from '../../../../styles/system-colors';
 
 const EndMessage: React.FC<EndMessageProps> = ({ onPressFinish, disabled }) => {
   const finishChat = () => {
@@ -22,7 +23,7 @@ const EndMessage: React.FC<EndMessageProps> = ({ onPressFinish, disabled }) => {
   const description =
     '상담은 만족스러우셨나요?\n24시간 후에 상담이 자동 종료됩니다!\n계속 상담을 진행하시면 자동 종료는 취소됩니다.';
   const footer = (
-    <Button type="block" color="error" disabled={disabled} onPress={finishChat}>
+    <Button disabled={disabled} onPress={finishChat}>
       지금 상담 종료하기
     </Button>
   );
--- src/atoms/Message/Templates/ReviewMessage/ReviewMessage.tsx
+++ src/atoms/Message/Templates/ReviewMessage/ReviewMessage.tsx
@@ -10,9 +10,7 @@ const ReviewMessage: React.FC<ReviewMessageProps> = ({ onPressReview }) => {
   const title = '리뷰를 남겨주세요!';
   const description = `상담은 만족스러우셨나요?\n${store}에 리뷰를 남겨주시면 닥터차의 발전에 큰 도움이 됩니다!`;
   const footer = (
-    <Button type="block" onPress={onPressReview}>
-      {`${store} 리뷰 남기기`}
-    </Button>
+    <Button onPress={onPressReview}>{`${store} 리뷰 남기기`}</Button>
   );
 
   return (
