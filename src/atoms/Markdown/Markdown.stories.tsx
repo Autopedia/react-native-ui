bc67322de5d1cfaf6c85371a02c4d1d683b90cd1
Pansok Hwang <pansokhwang@Pansoks-MacBook-Pro.local>
2022-06-20 19:16:38 +0900

    Edit Markdown

--- src/atoms/Markdown/Markdown.stories.tsx
+++ src/atoms/Markdown/Markdown.stories.tsx
@@ -80,10 +80,18 @@ storiesOf('Atoms/Markdown', module)
   .add('Pattern', () => {
     return (
       <>
-        <Markdown selectable>default Markdown</Markdown>
-        <Markdown selectable>**Bold Markdown**</Markdown>
-        <Markdown selectable>010-1234-5678</Markdown>
-        <Markdown selectable>https://doctor-cha.com/</Markdown>
+        <Markdown
+          selectable
+        >{`default Markdown\n**Bold Markdwon 굵은 글씨**\n010-1234-1234 전화번호도 기입\n\n[@brooky:https://google.com][@james:https://google.com] [#키워드:https://doctor-cha.com]\n\nhttps://www.doctor-cha.com\nwww.google.com 직접 입력된 주소\n[hello 안녕](https://doctor-cha.com/)`}</Markdown>
+      </>
+    );
+  })
+  .add('Heading', () => {
+    return (
+      <>
+        <Markdown
+          selectable
+        >{`# 제목1 Heading 1\ndefault Markdown\n## 제목2 Heading2\n### 제목3 Heading3 Test\n기본 텍스트 디폴트`}</Markdown>
       </>
     );
   });
--- src/atoms/Markdown/Markdown.tsx
+++ src/atoms/Markdown/Markdown.tsx
@@ -5,8 +5,17 @@ import ParsedText from 'react-native-parsed-text';
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 
-const BOLD_PATTERN = /\*\*(\S(.*?\S)?)\*\*/gm;
-const PHONENUMBER_PATTERN = /^\d{2,3}-\d{3,4}-\d{4}$/gm;
+const BOLD_PATTERN = /\*\*(\S(.*?\S)?)\*\*/gm; // **Bold**
+const ITALIC_PATTERN = /\*(\S(.*?\S)?)\*/gm; // *Italic*
+const BOLDITALIC_PATTERN = /\*\*\*(\S(.*?\S)?)\*\*\*/gm; // ***Italic***
+const PHONENUMBER_PATTERN = /\d{2,3}-\d{3,4}-\d{4}/; // 010-1234-1234
+const MENTION_PATTERN = /\[(@[^:]+):([^\]]+)\]/; // [@username:url]
+const HASHTAG_PATTERN = /\[(#[^:]+):([^\]]+)\]/; // [#keyword:url]
+const LINK_PATTERN = /\[([^\]]+)\]\(([^\)]+)\)/; // [text](url)
+const HEADING1_PATTERN = /^# (.*)$/gm; // # Heading1
+const HEADING2_PATTERN = /^## (.*)$/gm; // # Heading1
+const HEADING3_PATTERN = /^### (.*)$/gm; // # Heading1
+const HAS_SCHEME = /\:\/\//; // https://, drcha:// etc.
 
 interface MarkdownProps {
   textColor?: string;
@@ -31,14 +40,57 @@ export const Markdown: React.FC<MarkdownProps> = React.memo(
         style={mdTextStyle.text}
         parse={[
           {
+            pattern: HEADING1_PATTERN,
+            style: mdTextStyle.heading1,
+            renderText: renderSimpleText,
+          },
+          {
+            pattern: HEADING2_PATTERN,
+            style: mdTextStyle.heading2,
+            renderText: renderSimpleText,
+          },
+          {
+            pattern: HEADING3_PATTERN,
+            style: mdTextStyle.heading3,
+            renderText: renderSimpleText,
+          },
+          {
+            pattern: LINK_PATTERN,
+            style: mdTextStyle.url,
+            onPress: handleLinkPress,
+            renderText: renderSimpleText,
+          },
+          {
+            pattern: MENTION_PATTERN,
+            style: mdTextStyle.url,
+            onPress: handleMentionPress,
+            renderText: renderSimpleText,
+          },
+          {
+            pattern: HASHTAG_PATTERN,
+            style: mdTextStyle.url,
+            onPress: handleHashtagPress,
+            renderText: renderSimpleText,
+          },
+          {
             type: 'url',
             style: mdTextStyle.url,
-            onPress: handleUrlPress,
+            onPress: openLink,
+          },
+          {
+            pattern: BOLDITALIC_PATTERN,
+            style: mdTextStyle.boldItalic,
+            renderText: renderSimpleText,
           },
           {
             pattern: BOLD_PATTERN,
             style: mdTextStyle.bold,
-            renderText: renderBoldText,
+            renderText: renderSimpleText,
+          },
+          {
+            pattern: ITALIC_PATTERN,
+            style: mdTextStyle.italic,
+            renderText: renderSimpleText,
           },
           {
             pattern: PHONENUMBER_PATTERN,
@@ -64,8 +116,30 @@ const markdownTextStyle = (style: {
       fontSize: fonts.size[style.size],
       lineHeight: fonts.lineHeight[style.size],
     },
+    heading1: {
+      fontFamily: fonts.family.BOLD,
+      fontSize: fonts.size['L'],
+      lineHeight: fonts.lineHeight['L'],
+    },
+    heading2: {
+      fontFamily: fonts.family.MEDIUM,
+      fontSize: fonts.size['M'],
+      lineHeight: fonts.lineHeight['M'],
+    },
+    heading3: {
+      fontFamily: fonts.family.MEDIUM,
+      fontSize: fonts.size['S'],
+      lineHeight: fonts.lineHeight['S'],
+    },
     bold: {
       fontWeight: 'bold',
+    },
+    italic: {
+      fontStyle: 'italic',
+    },
+    boldItalic: {
+      fontWeight: 'bold',
+      fontStyle: 'italic',
     },
     url: {
       color: style.linkColor,
@@ -76,28 +150,18 @@ const markdownTextStyle = (style: {
   });
 };
 
-const openURL = async (url: string) => {
-  const valid = await Linking.canOpenURL(url);
-  if (valid) await Linking.openURL(url);
-  else
+const openLink = async (url: string) => {
+  let link = url;
+  if (!url.match(HAS_SCHEME)) {
+    link = `https://${url}`;
+  }
+  try {
+    await Linking.openURL(link);
+  } catch (error) {
     Alert.alert(
       '링크 접속 불가',
       '연결중 문제가 발생했습니다. 직접 복사하여 브라우저에서 시도해주세요',
     );
-};
-
-const handleUrlPress = async (url: string) => {
-  let link = url;
-  if (!url.startsWith('http')) {
-    link = `https://${url}`;
-    const valid = await Linking.canOpenURL(link);
-    if (valid) await Linking.openURL(link);
-    else {
-      link = `http://${url}`;
-      await openURL(link);
-    }
-  } else {
-    await openURL(link);
   }
 };
 
@@ -105,6 +169,21 @@ const handlePhonePress = (phoneNumber: string) => {
   Linking.openURL(`tel:${phoneNumber}`);
 };
 
-const renderBoldText = (matchingString: string) => {
-  return `${matchingString.replace(/\*\*(.*)\*\*/, '$1')}`;
+const handleMentionPress = (mentionMarkdown: string) => {
+  const mention = mentionMarkdown.match(MENTION_PATTERN);
+  openLink(mention[2]);
+};
+
+const handleHashtagPress = (hashtagMarkdown: string) => {
+  const hashtag = hashtagMarkdown.match(HASHTAG_PATTERN);
+  openLink(hashtag[2]);
+};
+
+const handleLinkPress = (linkMarkdown: string) => {
+  const link = linkMarkdown.match(LINK_PATTERN);
+  openLink(link[2]);
+};
+
+const renderSimpleText = (matchingString: string, matches: any) => {
+  return matches[1];
 };
